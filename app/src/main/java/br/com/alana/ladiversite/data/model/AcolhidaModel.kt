78e165c6e3eb5323cea8e5d4610098dc470aca12
May <dotta.may@gmail.com>
2023-04-24 19:36:33 -0300

    adicionado feature para aprovacao de sugestoes de acolhidas via admin

---
+++ app/src/main/java/br/com/alana/ladiversite/data/model/AcolhidaModel.kt
@@ -0,0 +1,12 @@
+package br.com.alana.ladiversite.data.model
+
+data class AcolhidaModel(
+    val nome: String? = null,
+    val acolhimento: String? = null,
+    val atividades: String? = null,
+    val email: String? = null,
+    val endereco: String? = null,
+    val publico: String? = null,
+    val telefone: String? = null,
+    var visivel: Boolean? = false
+)
