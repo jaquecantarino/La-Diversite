7f0007defc0509165f4c38fc6c5edbafbafb9aa4
May <dotta.may@gmail.com>
2023-04-24 19:21:32 -0300

    ajustes para ligacao sem tela de discagem

--- app/src/main/java/br/com/alana/ladiversite/utils/Discador.kt
+++ app/src/main/java/br/com/alana/ladiversite/utils/Discador.kt
@@ -1,15 +1,23 @@
 package br.com.alana.ladiversite.utils
 
+import android.Manifest
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat.startActivity
 
 class Discador {
     companion object {
-        fun ligarTelefone(telefone: String?, context: Context){
-            val phone = telefone
-            val intent = Intent(Intent.ACTION_DIAL, Uri.fromParts("tel", phone, null))
+        fun ligarTelefone(telefone: String?, context: Context, activity: Activity){
+            val intent = Intent(Intent.ACTION_CALL, Uri.fromParts("tel", telefone, null))
+
+            if (ActivityCompat.checkSelfPermission(context, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(activity, arrayOf(Manifest.permission.CALL_PHONE), 1)
+                return
+            }
             startActivity(context, intent, null)
         }
     }
