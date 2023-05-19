268e02480c090d59ce3a4c994b47470965c0cbae
Jaqueline Cantarino <jaquelinecantarino@outlook.com>
2023-05-19 13:16:19 -0300

    ajustes cadastro acolhida

--- app/src/main/java/br/com/alana/ladiversite/presentation/CadastroAcolhidaActivity.kt
+++ app/src/main/java/br/com/alana/ladiversite/presentation/CadastroAcolhidaActivity.kt
@@ -14,54 +14,48 @@ import br.com.alana.ladiversite.utils.PhoneNumberFormatter
 import br.com.alana.ladiversite.utils.Utils.Companion.removeNonDigits
 import br.com.alana.ladiversite.utils.ValidadorDados
 import com.example.mobcomponents.customtoast.CustomToast
-import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import java.lang.ref.WeakReference
 
 class CadastroAcolhidaActivity : AppCompatActivity() {
-
     private val binding: ActivityCadastroAcolhidaBinding by lazy { ActivityCadastroAcolhidaBinding.inflate(layoutInflater) }
     private val dataBaseFire = FirebaseFirestore.getInstance()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(binding.root)
-
         getDadosAcolhidaFromAdmin()
         phoneMask()
-
         binding.cadastrarCasaBtn.setOnClickListener {
             if (binding.edtCasa.text.isEmpty()){
                 binding.edtCasa.error = getText(R.string.casa_obrig)
                 camposWarning("Verifique o preenchimento do campo NOME DA CASA!")
             } else if(!ValidadorDados.isValidEmailOnly(binding.edtEmail.text.toString())){
+            } else if(!binding.edtEmail.text.isEmpty() && !ValidadorDados.isValidEmailOnly(binding.edtEmail.text.toString())){
                 binding.edtEmail.error = getText(R.string.email_valid)
                 camposWarning("Verifique o preenchimento do campo E-MAIL!")
             } else if (binding.edtTelefone.text.isEmpty() && binding.edtTelefone.text.length < 10) {
                 binding.edtTelefone.error = getText(R.string.telefone_obrig)
                 camposWarning("Verifique o preenchimento do campo TELEFONE!")
             } else {
-
                 val phone = removeNonDigits(binding.edtTelefone.text.toString())
                 val telefone = "+55 $phone"
-
                 if (isUpdate()){
                     val oldCasa = intent.getStringExtra("nomeCasa")
                     if (oldCasa.toString() != binding.edtCasa.text.toString())
                         apagaCasa(oldCasa.toString())
                 }
-
                 cadastrarAcolhida(
-                        binding.edtCasa.text.toString(),
-                        verifyYesNo(),
-                        binding.edtAtividades.text.toString(),
-                        binding.edtEmail.text.toString(),
-                        binding.edtEndereco.text.toString(),
-                        binding.edtPublico.text.toString(),
-                        telefone,
-                        verifyAdmin(),
-                        true
+                    binding.edtCasa.text.toString(),
+                    verifyYesNo(),
+                    binding.edtAtividades.text.toString(),
+                    binding.edtEmail.text.toString(),
+                    binding.edtEndereco.text.toString(),
+                    binding.edtPublico.text.toString(),
+                    telefone,
+                    verifyAdmin(),
+                    true
                 )
                 startActivity(AcolhidaActivity.startAcolhida(this).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP))
             }
@@ -70,22 +64,26 @@ class CadastroAcolhidaActivity : AppCompatActivity() {
 
     fun verifyYesNo(): String{
         return if (binding.sim.isChecked) "Sim" else "Não"
+        if (binding.sim.isChecked) {
+            return "Sim"
+        } else if (binding.nao.isChecked) {
+            return "Não"
+        } else {
+            return ""
+        }
     }
 
-    private fun verifyAdmin(): Boolean {
+    private fun verifyAdmin(): Boolean{
         val fireUser = FirebaseAuth.getInstance().currentUser?.uid
         return fireUser!! == "wWYLeLPAb5SMsBob5b7j6oRprwZ2"
     }
-
-    private fun isUpdate(): Boolean {
+    private fun isUpdate(): Boolean{
         return intent.hasExtra("nomeTela")
     }
-
     private fun apagaCasa(casa: String){
         dataBaseFire.collection("casas").document(casa).delete().addOnSuccessListener {}
             .addOnFailureListener { CustomToast.warning(this, it.toString()) }
     }
-
     private fun cadastrarAcolhida(casa: String, acolhimento: String, atividades: String, email: String, endere: String, publico: String, tel: String, visivel: Boolean, ativo: Boolean) {
         val casaMap = hashMapOf(
             "nome" to casa,
@@ -98,7 +96,6 @@ class CadastroAcolhidaActivity : AppCompatActivity() {
             "visivel" to visivel,
             "ativo" to ativo
         )
-
         dataBaseFire.collection("casas").document(casa)
             .set(casaMap).addOnCompleteListener {
                 CustomToast.success(this, "Enviado com sucesso!")
@@ -106,7 +103,6 @@ class CadastroAcolhidaActivity : AppCompatActivity() {
                 CustomToast.error(this, "Erro no envio dos dados!")
             }
     }
-
     private fun getDadosAcolhidaFromAdmin(){
         if (intent.hasExtra("nomeTela")){
             binding.cadastrarCasaBtn.text = getString(R.string.cadastrar_casa)
@@ -120,27 +116,22 @@ class CadastroAcolhidaActivity : AppCompatActivity() {
             binding.edtTelefone.setText(intent.getStringExtra("telefone"))
         }
     }
-
     fun verifyYesNoForAdmin(): Int{
         return if (intent.getStringExtra("acolhimento") == "Sim") binding.sim.id else binding.nao.id
     }
-
     fun phoneMask(){
         val editText: EditText = binding.edtTelefone
         val country = PhoneNumberFormatType.PT_BR
         val phoneFormatter = PhoneNumberFormatter(WeakReference(editText), country)
         editText.addTextChangedListener(phoneFormatter)
     }
-
     private fun camposWarning(msg: String){
         CustomToast.warning(this, msg)
     }
-
     companion object {
         fun startCadastroAcolhida(context: Context): Intent {
             return Intent(context, CadastroAcolhidaActivity::class.java)
         }
-
         fun startAcolhidaEdicao(context: Context,
                                 nomeTela: String?,
                                 nomeCasa: String?,
@@ -150,7 +141,7 @@ class CadastroAcolhidaActivity : AppCompatActivity() {
                                 endereco: String?,
                                 publico: String?,
                                 telefone: String?,
-                                ): Intent {
+        ): Intent {
             val intentDetalhes = Intent(context, CadastroAcolhidaActivity::class.java)
             intentDetalhes.apply {
                 putExtra("nomeTela", nomeTela)
