051ed862db174a373db477b187bb12d152166c98
Jaqueline Cantarino <jaquelinecantarino@outlook.com>
2023-05-22 12:19:11 -0300

    refactor tela de psicologos

--- app/src/main/java/br/com/alana/ladiversite/presentation/PsicologosActivity.kt
+++ app/src/main/java/br/com/alana/ladiversite/presentation/PsicologosActivity.kt
@@ -8,11 +8,11 @@ import android.view.WindowManager
 import br.com.alana.ladiversite.R
 import br.com.alana.ladiversite.databinding.ActivityPsicologosBinding
 import br.com.alana.ladiversite.utils.Utils
+import com.example.mobcomponents.customtoast.CustomToast
 
 class PsicologosActivity : AppCompatActivity() {
 
     private val binding: ActivityPsicologosBinding by lazy { ActivityPsicologosBinding.inflate(layoutInflater) }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
@@ -21,7 +21,24 @@ class PsicologosActivity : AppCompatActivity() {
         binding.cardPsico1.setOnClickListener { Utils.acessarLink(getString(R.string.psico_url1), this) }
         binding.cardPsico2.setOnClickListener { Utils.acessarLink(getString(R.string.psico_url2), this) }
         binding.cardPsico3.setOnClickListener { Utils.acessarLink(getString(R.string.psico_url3), this) }
+        initDiscadores()
+        initInstaPsicologos()
     }
+
+    private fun initDiscadores() {
+        binding.ps1Contato.setOnClickListener { CustomToast.success(this, "Ligando para Psicólogo...") }
+        binding.ps2Contato.setOnClickListener { CustomToast.success(this, "Ligando para Psicólogo...") }
+        binding.ps3Contato.setOnClickListener { CustomToast.success(this, "Ligando para Psicólogo...") }
+        binding.ps4Contato.setOnClickListener { CustomToast.success(this, "Ligando para Psicólogo...") }
+    }
+
+    private fun initInstaPsicologos() {
+        binding.ps1Insta.setOnClickListener { Utils.acessarLink("http://www.google.com.br", this) }
+        binding.ps2Insta.setOnClickListener { Utils.acessarLink("http://www.google.com.br", this) }
+        binding.ps3Insta.setOnClickListener { Utils.acessarLink("http://www.google.com.br", this) }
+        binding.ps4Insta.setOnClickListener { Utils.acessarLink("http://www.google.com.br", this) }
+    }
+
     companion object {
         fun startPsico(context: Context): Intent {
             return Intent(context, PsicologosActivity::class.java)
