77655e5267988845aa654c207a65c055ddbd6f57
Jaqueline Cantarino <jaquelinecantarino@outlook.com>
2023-06-07 15:16:55 -0300

    refatora psicologos

--- app/src/main/java/br/com/alana/ladiversite/presentation/PsicologosActivity.kt
+++ app/src/main/java/br/com/alana/ladiversite/presentation/PsicologosActivity.kt
@@ -4,8 +4,6 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.WindowManager
-import br.com.alana.ladiversite.R
 import br.com.alana.ladiversite.databinding.ActivityPsicologosBinding
 import br.com.alana.ladiversite.utils.Utils
 import com.example.mobcomponents.customtoast.CustomToast
@@ -13,17 +11,16 @@ import com.example.mobcomponents.customtoast.CustomToast
 class PsicologosActivity : AppCompatActivity() {
 
     private val binding: ActivityPsicologosBinding by lazy { ActivityPsicologosBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Utils.setFullScreen(this@PsicologosActivity)
         setContentView(binding.root)
 
-        binding.cardPsico1.setOnClickListener { Utils.acessarLink(getString(R.string.psico_url1), this) }
-        binding.cardPsico2.setOnClickListener { Utils.acessarLink(getString(R.string.psico_url2), this) }
-        binding.cardPsico3.setOnClickListener { Utils.acessarLink(getString(R.string.psico_url3), this) }
         initDiscadores()
         initInstaPsicologos()
     }
+
 
     private fun initDiscadores() {
         binding.ps1Contato.setOnClickListener { CustomToast.success(this, "Ligando para Psicólogo...") }
@@ -33,10 +30,10 @@ class PsicologosActivity : AppCompatActivity() {
     }
 
     private fun initInstaPsicologos() {
-        binding.ps1Insta.setOnClickListener { Utils.acessarLink("https://jaquecantarino.github.io/page-ladiversite/", this) }
-        binding.ps2Insta.setOnClickListener { Utils.acessarLink("https://jaquecantarino.github.io/page-ladiversite/", this) }
-        binding.ps3Insta.setOnClickListener { Utils.acessarLink("https://jaquecantarino.github.io/page-ladiversite/", this) }
-        binding.ps4Insta.setOnClickListener { Utils.acessarLink("https://jaquecantarino.github.io/page-ladiversite/", this) }
+        binding.ps1Insta.setOnClickListener { Utils.acessarLink("http://www.google.com.br", this) }
+        binding.ps2Insta.setOnClickListener { Utils.acessarLink("http://www.google.com.br", this) }
+        binding.ps3Insta.setOnClickListener { Utils.acessarLink("http://www.google.com.br", this) }
+        binding.ps4Insta.setOnClickListener { Utils.acessarLink("http://www.google.com.br", this) }
     }
 
     companion object {
